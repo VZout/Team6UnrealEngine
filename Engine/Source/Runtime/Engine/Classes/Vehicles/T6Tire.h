892c5b9e344f2879caccf2e5cb4da835d553fed0
ErwinT6 <erwin@team6-games.com>
2016-02-24 11:29:39 +0100

    Weg met oude rommel

--- Engine/Source/Runtime/Engine/Classes/Vehicles/T6Tire.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void T6TireShader(const void* shaderData, const PxF32 tireFriction, const PxF32 longSlip, const PxF32 latSlip, const PxF32 camber, const PxF32 wheelOmega, const PxF32 wheelRadius, const PxF32 recipWheelRadius, const PxF32 restTireLoad,
-					     const PxF32 normalisedTireLoad, const PxF32 tireLoad, const PxF32 gravity, const PxF32 recipGravity, PxF32& wheelTorque, PxF32& tireLongForceMag, PxF32& tireLatForceMag, PxF32& tireAlignMoment);
