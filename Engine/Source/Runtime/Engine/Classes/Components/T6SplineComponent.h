284175721ad75ce9673697ba9fdc0e09b19201b8
ErwinT6 <erwin@team6-games.com>
2016-03-03 01:42:27 +0100

    verplaatst

---
+++ Engine/Source/Runtime/Engine/Classes/Components/T6SplineComponent.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "T6SplineComponent.generated.h"
+
+UCLASS(ClassGroup = Utility, meta = (BlueprintSpawnableComponent))
+class ENGINE_API UT6SplineComponent : public UPrimitiveComponent{
+	GENERATED_BODY()
+};
